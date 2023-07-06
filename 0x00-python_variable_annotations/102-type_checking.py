496c3cff67d562b419495cfe1fc02eac2abce22d
stanley <stanley6453@gmail.com>
2023-07-07 00:13:57 +0100

    added file 0x00-python_variable_annotations/102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
-"""Use mypy to validate the following piece of code
-and apply any necessary changes.
+"""Use mypy to validate the following piece of code and apply any
+necessary changes
 """
 
-from typing import Tuple, List, Any
+
+from typing import Tuple, List
 
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List:
-    """Corrected annotations"""
+def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
+    """Use mypy to validate the following piece of code and apply any
+    necessary changes"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
