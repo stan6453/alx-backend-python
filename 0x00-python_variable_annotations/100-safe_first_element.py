e297fec40cde82b53696a03f3f7dc0a82f6b0ded
stanley <stanley6453@gmail.com>
2023-07-06 13:16:13 +0100

    added file 0x00-python_variable_annotations/100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -8,6 +8,8 @@ from typing import Union, Iterable, Sequence, List, Tuple, Any
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """Augment the following code with the correct duck-typed annotations
+    The types of the elements of the input are not know"""
     if lst:
         return lst[0]
     else:
