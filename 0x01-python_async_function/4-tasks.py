57eb64b8ae0d8f9f50033ddff80803dd3d1bbbcf
stanley <stanley6453@gmail.com>
2023-07-10 19:56:33 +0100

    modified file 0x01-python_async_function/4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-"""Take the code from wait_n and alter it into a new function task_wait_n.
-The code is nearly identical to wait_n except task_wait_random is being called."""
+"""Take the code from wait_n and alter it into a new function
+task_wait_n. The code is nearly identical to wait_n except
+task_wait_random is being called."""
 
 import asyncio
 from typing import List, Coroutine
@@ -9,6 +10,10 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Take the code from wait_n and alter it into a new function
+    task_wait_n. The code is nearly identical to wait_n except
+    task_wait_random is being called."""
+
     results: List[float] = []
     coroutines: List[Coroutine] = [add_to_list(n, max_delay, results)
                                    for n in range(n)]
@@ -20,5 +25,6 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
 
 async def add_to_list(n: int, max_delay: int,
                       results: List[float]):
+    """add return value of await_random to results when its done"""
     delay = await task_wait_random(max_delay)
     results.append(delay)
