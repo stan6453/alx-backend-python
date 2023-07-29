64175b7500ec1928fec52e88379b830246a333c7
stanley <stanley6453@gmail.com>
2023-07-29 19:12:32 +0100

    wrote unittest for GithubOrgClient._public_repos_url

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -2,16 +2,18 @@
 """Tests for clients.py"""
 
 import unittest
-from unittest.mock import patch, MagicMock
+from unittest.mock import patch, MagicMock, PropertyMock
 from client import GithubOrgClient
 from parameterized import parameterized
 from typing import (
     Mapping,
     Sequence,
     Any,
+    Union,
     Dict,
     Callable,
 )
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -31,3 +33,29 @@ class TestGithubOrgClient(unittest.TestCase):
 
         mock_get_json.assert_called_once_with(
             self.ORG_URL.format(org=org_name))
+
+    @parameterized.expand([
+        ("google",
+         {
+             "url": "https://api.github.com/orgs/google",
+             "repos_url": "https://api.github.com/orgs/google/repos",
+         }, "https://api.github.com/orgs/google/repos"),
+        ("abc", {
+            "message": "Not Found",
+            "documentation_url":
+            "https://docs.github.com/rest/orgs/orgs#get-an-organization"
+        }, KeyError)
+    ])
+    @patch('client.GithubOrgClient.org', new_callable=PropertyMock)
+    def test_public_repos_url(self, org_name: str, org_fun_result: Mapping,
+                              _public_repos_url_result,
+                              mock_org_fn: MagicMock):
+        """TODO: """
+        New_org = GithubOrgClient(org_name)
+        mock_org_fn.return_value = org_fun_result
+        if type(_public_repos_url_result) is str:
+            return_value = New_org._public_repos_url
+            self.assertEqual(return_value, _public_repos_url_result)
+        else:
+            with self.assertRaises(KeyError):
+                return_value = New_org._public_repos_url
